b09ca7772ac715d26884fe449cf9be70897fd0ba
PedroGabrielGuimaraesFernandes <36249685+PedroGabrielGuimaraesFernandes@users.noreply.github.com>
2024-08-14 08:55:12 -0300

    Foi criado exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+	//imprime algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	
+	//pausa antes de terminar o programa
+	system("pause");
+	return 0;
+}
